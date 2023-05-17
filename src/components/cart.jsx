004d24b07dbf0d4220c87a8715fce8b4b5fd4d5c
IceFire25 <alinai-play@mail.ru>
2023-05-17 15:55:10 +0300

    initial commit

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -2,7 +2,7 @@ function Cart(props) {
     const { quantity = 0, handleBasketShow = Function.prototype} = props;
     return (
         <div className="cart  light-green white-text lighten-2" onClick={handleBasketShow}>
-            <i class="material-icons">shopping_basket</i>
+            <i class="material-icons">shopping_cart</i>
             {quantity ? <span className="cart-quantity">{quantity}</span> : null}
         </div>
     );
